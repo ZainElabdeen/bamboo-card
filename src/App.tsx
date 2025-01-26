3d92cb67d451699d5e5707cf6836d3a82b234529
Zainelabdeen S.A Elgraeed <zainco30@gmail.com>
2025-01-26 21:32:07 +0400

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { Box, Container } from "@mui/material";
+
 import "./App.css";
+import Header from "./components/header";
 import GiftCardList from "./pages/gift-card-list";
 
 function App() {
   return (
-    <>
-      <GiftCardList />
-    </>
+    <Box
+      sx={{
+        backgroundColor: "#f9f9f9",
+        minHeight: "100vh",
+      }}
+    >
+      <Header />
+      <Container
+        maxWidth="lg"
+        sx={{
+          mt: 4,
+          pb: 4,
+        }}
+      >
+        <GiftCardList />
+      </Container>
+    </Box>
   );
 }
 
